4be3db07fdcf5b5952bd969fda1a07ce389055dc
Ong <Ong@gmail.com>
2020-06-09 18:18:42 +0700

    basic get method

---
+++ Drink.js
@@ -0,0 +1,20 @@
+const drink = [
+  {
+    id: 1,
+    name: 'tea',
+    email: 'tea@gmail.com',
+  },
+  {
+    id: 2,
+    name: 'coffee',
+    email: 'coffee@gmail.com',
+  },
+  {
+    id: 3,
+    name: 'soda',
+    email: 'soda@gmail.com',
+  }
+];
+
+module.exports = drink;
+
--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const path = require('path');
+const drink = require('./Drink');
 
 const app = express();
+
+app.get('/api/drink',(req,res) => {
+  res.json(drink);
+})
+
+app.get('/api/drink/:id',(req,res) => {
+  const found = drink.some(drinkById => drinkById.id === parseInt(req.params.id));
+
+  if(found){
+    res.json(drink.filter(drinkById => drinkById.id === parseInt(req.params.id)))
+  } else{
+    res.status(400).json({msg: `No drink with id of ${req.params.id}`})
+  }
+
+})
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(5000,() => { console.log('Server start') })
+
 
