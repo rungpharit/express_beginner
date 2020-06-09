b8d8c7e7f488d43dd7fc866b69f0ffafd7db4fe4
Ong <Ong@gmail.com>
2020-06-09 17:40:45 +0700

    create html

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
-app.get('/',(req,res) => {
-  res.send('test')
-})
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(5000,() => { console.log('Server start') })
 
