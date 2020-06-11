51334e49a99366e77e6e3effcf1a6fd8b9eefbd7
Ong <Ong@gmail.com>
2020-06-11 12:26:18 +0700

    drink

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const drinkRoute = require('./routes/drinking');
 
 const app = express();
 
+app.use(express.json()); //JSON
+app.use(express.urlencoded({ extended: false })); //FORM
+
 // Drink API Routes
 app.use('/api/drink', drinkRoute);
 
--- routes/drinking.js
+++ routes/drinking.js
@@ -19,4 +19,47 @@ router.get('/:id',(req,res) => {
   }
 })
 
+//Create drink
+router.post('/',(req,res) => {
+  const newDrink = {
+    id: drink.length + 1,
+    name: req.body.name,
+    email: req.body.email
+  };
+
+  if (!newDrink.name || !newDrink.email) {
+    return res.status(400).json({ msg: 'Please include a name and email' });
+  }
+
+  drink.push(newDrink);
+  res.json(drink);
+
+})
+
+//Update Member
+router.put('/:id',(req,res) => {
+  const found = drink.some(drinkById => drinkById.id === parseInt(req.params.id));
+
+  if(found){
+    const updDrink = req.body;
+    drink.forEach(drinkById => {
+      if(drinkById.id === parseInt(req.params.id)){
+        drinkById.name = updDrink.name ? updDrink.name : drink.name;
+        drinkById.email = updDrink.email ? updDrink.email : drink.email;
+        res.json({msg:'drink has been updated', drink:drink})
+      }
+    })
+  }else{
+    res.status(400).json({msg:`No drink with id of${req.params.id}`})
+  }
+});
+
+
+
+
+
+
+
 module.exports = router;
+
+
